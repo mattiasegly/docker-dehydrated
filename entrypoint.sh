5aa530793bb6ae0821980e4b7a39628bd45a4340
mattiasegly <48134992+mattiasegly@users.noreply.github.com>
2019-03-08 17:46:10 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Create dehydrated configuration file..."
+cat > /dehydrated/config << EOF
+CA="https://acme-v02.api.letsencrypt.org/directory"
+#CA="https://acme-staging-v02.api.letsencrypt.org/directory"
+CHALLENGETYPE="dns-01"
+DOMAINS_TXT="\${BASEDIR}/domains.txt"
+CONTACT_EMAIL=""$LETSENCRYPT_EMAIL""
+HOOK="\${BASEDIR}/hooks/loopia/loopia_hook.sh"
+CERTDIR="/letsencrypt"
+EOF
+
+echo "Create domain list file..."
+cat > /dehydrated/domains.txt << EOF
+$LETSENCRYPT_DOMAIN *.$LETSENCRYPT_DOMAIN
+EOF
+
+echo "Create loopia auth file..."
+cat > /dehydrated/hooks/loopia/loopia-api-user.auth << EOF
+user=$LOOPIA_USER
+pass=$LOOPIA_PASSWORD
+#user='$LOOPIA_USER'
+#pass='$LOOPIA_PASSWORD'
+EOF
+
+echo "Print dehydrated configuration values..."
+/dehydrated/dehydrated --env
+
+echo "Finished! Run container."
+exec "$@"
